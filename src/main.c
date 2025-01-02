22017ca88891f0f24a05e50b3bbaf476bc172e31
surajkareppagol <surajkareppagol.dev@gmail.com>
2025-01-02 08:08:51 +0530

    🪴 Add check for command line arguments
    - Add help message
    - Add check for command line arguments

--- src/main.c
+++ src/main.c
@@ -5,6 +5,11 @@
 int main(int argc, char **argv) {
   printf("libcsv - Yet another CSV library\n");
 
+  if (argc < 2) {
+    printf("Usage: %s [file.csv]\n", argv[0]);
+    return 1;
+  }
+
   CSV_METADATA *metadata = NULL;
 
   CSV_LIST *csv_list = csv_import(argv[1], &metadata);
@@ -18,7 +23,7 @@ int main(int argc, char **argv) {
   printf("\n");
 
   for (int i = 0; i < metadata->fields; i++) {
-    CSV_STRING_BLOCK *block = csv_list->field_list[i]->string_block;
+    CSV_STRING_BLOCK *block = csv_list->field_list[i]->string_block_head;
 
     while (block != NULL) {
       printf("| %10s | ", block->data);
