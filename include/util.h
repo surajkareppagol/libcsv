7431608cee560b7594313ee2e2233897e54661af
surajkareppagol <surajkareppagol.dev@gmail.com>
2025-01-09 17:42:28 +0530

    up: Add more util functions
    - Add util_string_to_int function prototypes
    - Add util_string_to_double function prototypes

--- include/util.h
+++ include/util.h
@@ -2,6 +2,31 @@
 #define UTIL
 
 /************ UTILITY API ************/
+
+/**
+ * @brief Remove left and right spaces
+ *
+ * @param string
+ * @return char*
+ */
 char *util_trim_string(char *string);
+
+/**
+ * @brief Convert string to number
+ *
+ * @param string
+ * @param data
+ * @return int
+ */
+int util_string_to_number(char *string, int *data);
+
+/**
+ * @brief Convert string to double
+ *
+ * @param string
+ * @param data
+ * @return int
+ */
+int util_string_to_double(char *string, double *data);
 
 #endif
